5146d312ac82fcc7a94ca43800670bec956db70d
bilalahmad9755 <115340899+bilalahmad9755@users.noreply.github.com>
2023-09-26 14:21:46 +0500

    Update lib.rs
    deposit SOL

--- programs/anchorcontracts/src/lib.rs
+++ programs/anchorcontracts/src/lib.rs
@@ -1,15 +1,16 @@
 use anchor_lang::prelude::*;
-
+use anchor_lang::system_program;
 declare_id!("7315G26yoNcSxQtmMT25k86ktZ319SecsPPzxeZRmN9J");
-
 #[program]
 pub mod anchorcontracts {
     use super::*;
 
+    const DEPOSIT_AMOUNT: u64 = 100000000; // 0.1 SOL
+
     pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
-    pub fn create_user_deposit(_ctx: Context<CreateUserDeposit>) -> Result<()>
+    pub fn initialize_user_deposit(_ctx: Context<CreateUserDeposit>) -> Result<()>
     {
         Ok(())
     }
@@ -19,6 +20,26 @@ pub mod anchorcontracts {
         _ctx.accounts.account.amount = data.amount;
         Ok(())
     }
+    
+    pub fn initialize_derived_account(_ctx: Context<CreateDerivedAccount>) ->  Result<()>{
+        Ok(())
+    }
+
+    pub fn deposit_sol(_ctx: Context<DepositSOL>) -> Result<()>{
+        let cpi_context = CpiContext::new
+        (
+            _ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer 
+            {
+                from: _ctx.accounts.user.to_account_info().clone(),
+                to: _ctx.accounts.pda_account.to_account_info().clone(),
+            },
+        );
+        system_program::transfer(cpi_context, DEPOSIT_AMOUNT)?;
+
+        Ok(())   
+    }
+
 }
 
 #[derive(Accounts)]
@@ -28,7 +49,7 @@ pub struct Initialize {}
 #[derive(Accounts)]
 pub struct CreateUserDeposit<'info>{
     #[account(init, payer=user, space=8+40)]
-    pub data: Box<Account<'info, UserDeposit>>,
+    pub account: Account<'info, UserDeposit>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
@@ -47,4 +68,33 @@ pub struct Deposit<'info>{
 pub struct UserDeposit
 {
     pub amount: u64,
+}
+
+#[account]
+#[derive(Default)]
+pub struct DerivedAccount
+{
+    pub user: Pubkey,
+    pub balance: u64,
+    pub bump: u8
+}
+
+#[derive(Accounts)]
+pub struct CreateDerivedAccount<'info>
+{
+    #[account(init, payer=user, space=8+32+64+8, seeds=[b"MY_SEED", user.key().as_ref()], bump)]
+    pub pda_account : Account<'info, DerivedAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DepositSOL<'info>
+{
+    #[account(mut, seeds=[b"MY_SEED", user.key().as_ref()], bump)]
+    pub pda_account : Account<'info, DerivedAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
